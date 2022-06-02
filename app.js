d633a2fc5998762232bba41ea7db072d60e5aa35
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-06-03 01:31:02 +0500

    Create POST request

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 //-- Expess is a functoin that upon calling add methods to app variable
 const app = express();
 
+app.use(express.json());
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -16,6 +18,28 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'sucess',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
