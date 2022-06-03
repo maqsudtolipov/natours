d6c5df2ea75ba9a11585526ef27f1b4cf7f332f4
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-06-03 16:38:48 +0500

    Create mounting routes

--- app.js
+++ app.js
@@ -144,17 +144,17 @@ const deleteUser = (req, res) => {
 };
 
 // 3 ROUTES
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-app.route('/api/v1/tour/:id').get(getTour).patch(updateTour).delete(deleteTour);
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 // 4 SERVER
 
